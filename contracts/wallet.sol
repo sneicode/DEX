08632c48ad67a6e4657b9f580ad6935cf8a5e5fc
sneicode <68944040+sneicode@users.noreply.github.com>
2021-07-08 11:09:23 +0200

    import openzeppelin contracts and begin  wallet.sol-coding

---
+++ contracts/wallet.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: SPDX-License
+
+pragma solidity ^0.8.0;
+
+import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "../node_modules/@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract Wallet {
+    using SafeMath for uint256;
+
+    struct Token {
+        bytes32 ticker;
+        address tokenAddress;
+    }
+
+    mapping(bytes32 => Token) public tokenMapping;
+    bytes32[] public tokenList;
+
+
+    mapping(address => mapping(bytes32 => uint256)) public balances;
+
+    function addToken(bytes32 ticker, address tokenAddress) external {
+        tokenMapping[ticker] = Token(ticker, tokenAddress);
+        tokenList.push(ticker);
+    }
+
+    function deposit(uint amount, bytes32 ticker) external {
+
+    }
+
+    function withdraw(uint amount, bytes32 ticker) external {
+        require(tokenMapping[ticker].tokenAddress != address(0), "token doesn't exist");
+        require(balances[msg.sender][ticker] >= amount, "insufficient balance");
+
+        balances[msg.sender][ticker] = balances[msg.sender][ticker].sub(amount);
+        IERC20(tokenMapping[ticker].tokenAddress).transfer(msg.sender, amount);
+    }
+
+}
