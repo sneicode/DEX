e3ca4a38cf7f614a226968cd6e76d4c81667c80c
sneicode <68944040+sneicode@users.noreply.github.com>
2021-07-08 11:50:00 +0200

    added modifiers tokenExists and Ownable from @openzeppelin

--- contracts/wallet.sol
+++ contracts/wallet.sol
@@ -4,8 +4,9 @@ pragma solidity ^0.8.0;
 
 import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "../node_modules/@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
-contract Wallet {
+contract Wallet is Ownable {
     using SafeMath for uint256;
 
     struct Token {
@@ -19,17 +20,23 @@ contract Wallet {
 
     mapping(address => mapping(bytes32 => uint256)) public balances;
 
-    function addToken(bytes32 ticker, address tokenAddress) external {
+    modifier tokenExists(bytes32 ticker){
+        require(tokenMapping[ticker].tokenAddress != address(0), "Token doesn't exist");
+        _; 
+    }
+
+    function addToken(bytes32 ticker, address tokenAddress) onlyOwner external {
         tokenMapping[ticker] = Token(ticker, tokenAddress);
         tokenList.push(ticker);
     }
 
-    function deposit(uint amount, bytes32 ticker) external {
-
+    function deposit(uint amount, bytes32 ticker) tokenExists(ticker) external{
+        IERC20(tokenMapping[ticker].tokenAddress).transferFrom(msg.sender, address(this), amount);
+        balances[msg.sender][ticker] = balances[msg.sender][ticker].add(amount);
+        
     }
 
-    function withdraw(uint amount, bytes32 ticker) external {
-        require(tokenMapping[ticker].tokenAddress != address(0), "token doesn't exist");
+    function withdraw(uint amount, bytes32 ticker) tokenExists(ticker) external {
         require(balances[msg.sender][ticker] >= amount, "insufficient balance");
 
         balances[msg.sender][ticker] = balances[msg.sender][ticker].sub(amount);
